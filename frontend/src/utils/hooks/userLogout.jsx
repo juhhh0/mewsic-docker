9cf95a145f2f59e2456fa8e6c940335e394cf422
Joris Mansion <juhh@juhh-core.lan>
2023-12-27 17:26:06 +0100

    clear player on logout

--- frontend/src/utils/hooks/userLogout.jsx
+++ frontend/src/utils/hooks/userLogout.jsx
@@ -1,10 +1,13 @@
 import { useContext } from "react";
 import { UserContext } from "../../contexts/userContext.jsx";
+import { PlayerContext } from "../../contexts/playerContext.jsx";
 
 export const UserLogout = () => {
   const { setUser } = useContext(UserContext);
+  const { setPlaylist } = useContext(PlayerContext)
 
   const logout = () => {
+    setPlaylist([])
     localStorage.removeItem("user");
     setUser(null);
   };
