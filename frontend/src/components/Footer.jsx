a44d9084394c930e85c95bf9da6ca8b73541a6e6
Joris Mansion <juhh@juhh-core.lan>
2024-01-19 07:55:18 +0100

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,8 +5,13 @@ import { UserContext } from "../contexts/userContext";
 export default function Footer() {
   const { user } = useContext(UserContext);
   return (
-    <footer>
-      {user && <Link to="/contact">having trouble? contact us</Link>}
+    <footer className="flex">
+      <Link to={"/cgu"}>Terms of service</Link>
+      {user && (
+        <>
+      <span>-</span>
+      <Link to="/contact">Having trouble? contact us</Link>
+        </>)}
     </footer>
   );
 }
